70becb8fabaa5fd453e45b3bca2a635f144f747b
Nayem5522 <sahidhasannayembd@gmail.com>
2024-09-20 21:59:29 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -80,6 +80,11 @@ BUTTONS2 = {}
 #                 await auto_filter(client, message)
 #         except Exception as e:
 #             logger.error(f"Chat Not verified : {e}") 
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def give_filter(client, message):
+    k = await manual_filters(client, message)
+    if k == False:
+        await auto_filter(client, message)
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
