4854773d5f5b64d546a53a4659a632efe38aaf5b
Nayem5522 <sahidhasannayembd@gmail.com>
2024-09-21 11:15:03 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,15 +39,9 @@ from lazybot import LazyPrincessBot
 from util.keepalive import ping_server
 from lazybot.clients import initialize_clients
 
-
 PORT = "8080"
 LazyPrincessBot.start()
 loop = asyncio.get_event_loop()
-
-@LazyPrincessBot.on_message(filters.private | filters.group)
-async def auto_delete(client, message):
-    await asyncio.sleep(10)  # ১০ সেকেন্ড পর মেসেজ ডিলিট হবে
-    await client.delete_messages(chat_id=message.chat.id, message_ids=message.message_id)
 
 async def Lazy_start():
     print('\n')
